e6293fd7cf92e931599f8b44368a14bbfd85aeab
Sean Frischmann <stfrischmann@gmail.com>
2015-12-02 21:24:01 -0500

    adding gitignore

--- advent1.py
+++ advent1.py
@@ -3,15 +3,19 @@ data = data.read()
 
 floor = 0
 position = 1
+finalPos = 0
 
 for x in data:
     if x == '(':
         floor += 1
+
     elif x == ')':
         floor -= 1
+
     if floor == -1:
         finalPos = position
-        break
-    position += 1
+
+    if finalPos == 0:
+        position += 1
 
 print (position)
